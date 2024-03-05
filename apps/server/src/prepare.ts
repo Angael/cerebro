16fb464b6c473a50cb2de50d1a2ab1f3901b842c
Angael <krzysztofwidacki@gmail.com>
2024-03-05 20:36:06 +0100

    prepare dirs optimization

--- apps/server/src/prepare.ts
+++ apps/server/src/prepare.ts
@@ -6,13 +6,16 @@ import {
   THUMBNAILS_DIR,
   UPLOADS_DIR,
 } from './utils/consts.js';
+import logger from '@/utils/log.js';
 
 (async () => {
   await fs.emptyDir(TEMP_DIR).catch((e) => {});
-  await fs.mkdir(UPLOADS_DIR, { recursive: true });
-  await fs.mkdir(DOWNLOADS_DIR, { recursive: true });
-  await fs.mkdir(THUMBNAILS_DIR, { recursive: true });
-  await fs.mkdir(OPTIMIZATION_DIR, { recursive: true });
+  await Promise.all([
+    fs.mkdir(UPLOADS_DIR, { recursive: true }),
+    fs.mkdir(DOWNLOADS_DIR, { recursive: true }),
+    fs.mkdir(THUMBNAILS_DIR, { recursive: true }),
+    fs.mkdir(OPTIMIZATION_DIR, { recursive: true }),
+  ]);
 
-  console.log('Prepared directories');
+  logger.info('Prepared directories');
 })();
