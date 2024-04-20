0934edff0a2b89cf9a850c512d7ac9f81a9c2a39
Angael <krzysztofwidacki@gmail.com>
2024-04-20 11:48:19 +0200

    move query client

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,15 +1,8 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import * as React from 'react';
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5 * 1000,
-    },
-  },
-});
+import { queryClient } from '@/utils/queryClient';
 
 export function Providers(props: { children: React.ReactNode }) {
   return <QueryClientProvider client={queryClient}>{props.children}</QueryClientProvider>;
