01551be9687d5c2a30659b542b3fcd0d4ee9c36e
Angael <krzysztofwidacki@gmail.com>
2024-04-26 00:02:18 +0200

    WIP changes

--- apps/server/src/routes/admin/admin.service.ts
+++ apps/server/src/routes/admin/admin.service.ts
@@ -1,6 +1,7 @@
 import { db } from '@cerebro/db';
 import { AdminAllUsers, AdminUserPreview_Endpoint } from '@cerebro/shared';
 import { getLimitsForUser, getSpaceUsedByUser } from '@/routes/user/user.service.js';
+import { sql } from 'kysely';
 
 export const getAllUsers = async (): Promise<AdminAllUsers> => {
   return await db.selectFrom('user').select(['id', 'email', 'type']).execute();
@@ -22,4 +23,33 @@ export const getUserPreview = async (userId: string): Promise<AdminUserPreview_E
     maxSpace: max,
     itemCount: Number(itemCountStr?.itemCount),
   };
+};
+
+// SELECT user_id, SUM(size) AS total
+// FROM (SELECT id, user_id
+//       FROM item) AS user_items
+//          LEFT JOIN (SELECT item_id, size
+//                     FROM image
+//                     UNION ALL
+//                     SELECT item_id, size
+//                     FROM video
+//                     UNION ALL
+//                     SELECT item_id, size
+//                     FROM thumbnail) AS combined_sizes ON user_items.id = combined_sizes.item_id
+// GROUP BY user_items.user_id;
+
+export const getAllUsersSpaceUsage = async (): Promise<> => {
+  // TODO: FINISH typing this and use it in front
+  const rows = await sql`SELECT user_id, SUM(size) AS total
+             FROM (SELECT id, user_id
+                   FROM item) AS user_items
+                      LEFT JOIN (SELECT item_id, size
+                                 FROM image
+                                 UNION ALL
+                                 SELECT item_id, size
+                                 FROM video
+                                 UNION ALL
+                                 SELECT item_id, size
+                                 FROM thumbnail) AS combined_sizes ON user_items.id = combined_sizes.item_id
+             GROUP BY user_items.user_id;`.execute(db);
 };
