a927434c6356e05d77cc171e2f3b768addab6f18
Angael <Krzysztofwidacki@gmail.com>
2024-04-29 23:30:06 +0200

    prefetch pagination; initialData on item queries (#7)

--- apps/web/src/lib/item-grid/item-thumb/ItemThumb.tsx
+++ apps/web/src/lib/item-grid/item-thumb/ItemThumb.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import css from './ItemThumb.module.scss';
 import { getGridSpan } from './getGridSpan';
-import { mdiClockOutline, mdiEyeOff } from '@mdi/js';
+import { mdiAlertCircleOutline, mdiClockOutline, mdiEyeOff } from '@mdi/js';
 import { Icon } from '@mdi/react';
 import { FrontItem } from '@cerebro/shared';
 import clsx from 'clsx';
 import Link from 'next/link';
+import { Paper } from '@mantine/core';
 
 interface IProps {
   item: FrontItem;
 }
 
 const ItemThumb = ({ item }: IProps) => {
+  const [isThumbnailError, setIsThumbnailError] = useState(false);
   const thumbnailSrc = item.thumbnail ?? item.icon ?? '';
   const gridSpanClass = getGridSpan(item);
 
@@ -20,13 +22,25 @@ const ItemThumb = ({ item }: IProps) => {
     <Link href={`/item?itemId=${item.id}`} className={clsx(css.itemBtn, gridSpanClass)}>
       {item.private && <Icon path={mdiEyeOff} size={1} className={css.private} />}
       <div className={css.thumbnailContainer}>
-        {!thumbnailSrc ? (
+        {isThumbnailError && (
+          <Paper className={css.centerContainer} style={{ textAlign: 'center' }}>
+            <Icon path={mdiAlertCircleOutline} size={2} color={'white'} />
+            <p>Thumbnail error</p>
+          </Paper>
+        )}
+
+        {!isThumbnailError && !thumbnailSrc ? (
           <div className={css.centerContainer} style={{ backgroundColor: 'grey' }}>
             <Icon path={mdiClockOutline} size={2} color={'white'} />
             <p>Thumbnail not ready...</p>
           </div>
         ) : (
-          <img alt={'item ' + item.id} src={thumbnailSrc} className={clsx(css.thumbnail)} />
+          <img
+            alt={'item ' + item.id}
+            src={thumbnailSrc}
+            className={clsx(css.thumbnail)}
+            onError={() => setIsThumbnailError(true)}
+          />
         )}
       </div>
     </Link>
