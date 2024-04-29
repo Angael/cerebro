2ea90c7281f9d8b64cc41c465948daa4221a7628
Angael <Krzysztofwidacki@gmail.com>
2024-04-29 22:29:58 +0200

    Cloudflare r2 (#6)
    * gitignore, env template for cf
    
    * remove idea folder from repo
    
    * aws s3 sdk
    
    * s3 system working with Cloudflare's R2
    
    * better error for create bucket
    
    * remove useless folder

--- apps/server/src/aws/s3.ts
+++ apps/server/src/aws/s3.ts
@@ -1,23 +1,21 @@
-import AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
 import { S3CreateBucket } from './s3-helpers.js';
 import { env } from '@/utils/env.js';
 import logger from '@/utils/log.js';
 
-AWS.config.update({
-  apiVersion: 'latest',
-  region: env.AWS_REGION,
-  accessKeyId: env.AWS_KEY,
-  secretAccessKey: env.AWS_SECRET,
+export const s3 = new S3Client({
+  region: 'auto',
+  endpoint: env.CF_S3_ENDPOINT,
+
   credentials: {
-    accessKeyId: env.AWS_KEY,
-    secretAccessKey: env.AWS_SECRET,
+    accessKeyId: env.CF_KEY_ID,
+    secretAccessKey: env.CF_KEY_SECRET,
   },
 });
 
-export const s3 = new AWS.S3();
-
 try {
-  await S3CreateBucket(env.AWS_BUCKET_NAME as string);
-} catch (e) {
-  logger.error('Failed to create bucket', e);
+  // Is this code even useful?
+  await S3CreateBucket(env.CF_BUCKET_NAME as string);
+} catch (e: any) {
+  logger.error('Failed to create bucket %s', e?.message);
 }
--- apps/server/src/utils/env.ts
+++ apps/server/src/utils/env.ts
@@ -1,15 +1,5 @@
 import z from 'zod';
 
-//      NODE_ENV: 'development' | 'production';
-//       PORT?: string;
-//       AWS_USER: string;
-//       AWS_KEY: string;
-//       AWS_SECRET: string;
-//       AWS_BUCKET_NAME: string;
-//       AWS_REGION: string;
-//       DATABASE_URL: string;
-//       MOCK_UPLOADS: 'true' | 'false' | undefined;
-
 export const env = z
   .object({
     isProd: z.boolean(),
@@ -17,12 +7,12 @@ export const env = z
     YT_DLP_PATH: z.string(),
     PORT: z.number(),
     CORS_URL: z.string().optional(),
-    AWS_USER: z.string(),
-    AWS_KEY: z.string(),
-    AWS_SECRET: z.string(),
-    AWS_BUCKET_NAME: z.string(),
-    AWS_REGION: z.string(),
     MOCK_UPLOADS: z.boolean(),
+    CF_S3_ENDPOINT: z.string(),
+    CF_KEY_ID: z.string(),
+    CF_KEY_SECRET: z.string(),
+    CF_BUCKET_NAME: z.string(),
+    CF_CDN_URL: z.string(),
   })
   .parse({
     NODE_ENV: process.env.NODE_ENV,
@@ -30,10 +20,10 @@ export const env = z
     YT_DLP_PATH: process.env.YT_DLP_PATH || 'yt-dlp',
     PORT: Number(process.env.PORT || '3000'),
     CORS_URL: process.env.CORS_URL,
-    AWS_USER: process.env.AWS_USER,
-    AWS_KEY: process.env.AWS_KEY,
-    AWS_SECRET: process.env.AWS_SECRET,
-    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-    AWS_REGION: process.env.AWS_REGION,
     MOCK_UPLOADS: process.env.MOCK_UPLOADS === 'true',
+    CF_S3_ENDPOINT: process.env.CF_S3_ENDPOINT,
+    CF_KEY_ID: process.env.CF_KEY_ID,
+    CF_KEY_SECRET: process.env.CF_KEY_SECRET,
+    CF_BUCKET_NAME: process.env.CF_BUCKET_NAME,
+    CF_CDN_URL: process.env.CF_CDN_URL,
   });
