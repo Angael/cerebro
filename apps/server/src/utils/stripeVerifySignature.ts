56c878cb3b77a033d9edadbc36235c8ca650fa54
angael <krzysztofwidacki@gmail.com>
2024-05-01 20:46:21 +0200

    add migration to add stripe

--- apps/server/src/utils/stripeVerifySignature.ts
+++ apps/server/src/utils/stripeVerifySignature.ts
@@ -1,12 +1,13 @@
+import type { Request } from 'express';
 import { stripe } from '@/stripe.js';
 import logger from '@/utils/log.js';
 import { HttpError } from '@/utils/errors/HttpError.js';
 
-export const stripeVerifySignature = async (req: any) => {
+export const stripeVerifySignature = async (req: Request) => {
   const signature = req.headers['stripe-signature'];
   const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET!;
   try {
-    return stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
+    return stripe.webhooks.constructEvent(req.body, signature as any, endpointSecret);
   } catch (err) {
     logger.error('Stripe signature verification failed! Someone hacking?');
     throw new HttpError(400, 'Stripe signature verification failed!');
