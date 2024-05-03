ca9b1fc106f9c3ca64693ea35218286caa305c9f
angael <krzysztofwidacki@gmail.com>
2024-05-03 12:18:17 +0200

    fix STRIPE_WEBHOOK_SECRET

--- apps/server/src/utils/stripeVerifySignature.ts
+++ apps/server/src/utils/stripeVerifySignature.ts
@@ -2,10 +2,12 @@ import type { Request } from 'express';
 import { stripe } from '@/stripe.js';
 import logger from '@/utils/log.js';
 import { HttpError } from '@/utils/errors/HttpError.js';
+import { env } from '@/utils/env.js';
 
 export const stripeVerifySignature = async (req: Request) => {
   const signature = req.headers['stripe-signature'];
-  const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET!;
+  const endpointSecret = env.STRIPE_WEBHOOK_SECRET;
+
   try {
     return stripe.webhooks.constructEvent(req.body, signature as any, endpointSecret);
   } catch (err) {
