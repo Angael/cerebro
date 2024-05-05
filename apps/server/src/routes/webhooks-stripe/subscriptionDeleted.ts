6a5ca01eb833e3bbc883b2ef241286d882233af1
Angael <krzysztofwidacki@gmail.com>
2024-05-05 19:35:20 +0200

    update .env.template

--- apps/server/src/routes/webhooks-stripe/subscriptionDeleted.ts
+++ apps/server/src/routes/webhooks-stripe/subscriptionDeleted.ts
@@ -4,23 +4,24 @@ import { db } from '@cerebro/db';
 import invariant from 'tiny-invariant';
 
 const sessionDeletedObjectZod = z.object({
-  subscription: z.object({ id: z.string() }),
+  object: z.literal('subscription'),
+  id: z.string(),
 });
 
 export const subscriptionDeleted = async (event: Stripe.Event) => {
   invariant(event.type === 'customer.subscription.deleted', 'Invalid event type');
-  const { subscription } = sessionDeletedObjectZod.parse(event.data.object);
+  const { id } = sessionDeletedObjectZod.parse(event.data.object);
 
   await db
     .updateTable('stripe_customer')
     .set({ active_plan: null, subscription_id: null })
-    .where('subscription_id', '=', subscription.id)
+    .where('subscription_id', '=', id)
     .execute();
 
   const { user_id } = await db
     .selectFrom('stripe_customer')
     .select('user_id')
-    .where('subscription_id', '=', subscription.id)
+    .where('subscription_id', '=', id)
     .executeTakeFirstOrThrow();
 
   await db.updateTable('user').set({ type: 'FREE' }).where('id', '=', user_id).execute();
