1f9343351757956eebd94be39a456a35cb4c80bd
Angael <krzysztofwidacki@gmail.com>
2024-05-05 12:20:40 +0200

    useUserLimits

--- apps/web/src/lib/used-space/UsedSpace.tsx
+++ apps/web/src/lib/used-space/UsedSpace.tsx
@@ -1,23 +1,11 @@
 'use client';
 import React from 'react';
 import numeral from 'numeral';
-import { useQuery } from '@tanstack/react-query';
-import { GetUploadLimits } from '@cerebro/shared';
-import { API } from '@/utils/API';
-import { QUERY_KEYS } from '@/utils/consts';
 import { LoadingOverlay, Paper, Progress, Stack, Text } from '@mantine/core';
+import { useUserLimits } from '@/utils/hooks/useUserLimits';
 
-type Props = {
-  initialValue?: GetUploadLimits;
-};
-
-const UsedSpace = (props: Props) => {
-  const { data, isFetching, isFetched, isError } = useQuery({
-    queryKey: [QUERY_KEYS.uploadLimits],
-    queryFn: async () => API.get<GetUploadLimits>('/user/limits').then((res) => res.data),
-    refetchOnWindowFocus: true,
-    initialData: props.initialValue,
-  });
+const UsedSpace = () => {
+  const { data } = useUserLimits();
 
   const usageString =
     numeral(data?.bytes.used).format('0 b') + ' out of ' + numeral(data?.bytes.max).format('0 b');
---
+++ apps/web/src/utils/hooks/useUserLimits.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/utils/consts';
+import { API } from '@/utils/API';
+import { GetUploadLimits } from '@cerebro/shared';
+
+export const useUserLimits = () =>
+  useQuery({
+    queryKey: [QUERY_KEYS.uploadLimits],
+    queryFn: async () => API.get<GetUploadLimits>('/user/limits').then((res) => res.data),
+    refetchOnWindowFocus: true,
+  });
