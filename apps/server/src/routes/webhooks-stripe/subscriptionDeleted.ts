617526ec816fe6259986031d3073bf0ccad00c7a
Angael <krzysztofwidacki@gmail.com>
2024-05-05 20:26:12 +0200

    fix sub delete

--- apps/server/src/routes/webhooks-stripe/subscriptionDeleted.ts
+++ apps/server/src/routes/webhooks-stripe/subscriptionDeleted.ts
@@ -12,17 +12,18 @@ export const subscriptionDeleted = async (event: Stripe.Event) => {
   invariant(event.type === 'customer.subscription.deleted', 'Invalid event type');
   const { id } = sessionDeletedObjectZod.parse(event.data.object);
 
-  await db
-    .updateTable('stripe_customer')
-    .set({ active_plan: null, subscription_id: null })
-    .where('subscription_id', '=', id)
-    .execute();
-
   const { user_id } = await db
     .selectFrom('stripe_customer')
     .select('user_id')
     .where('subscription_id', '=', id)
     .executeTakeFirstOrThrow();
 
-  await db.updateTable('user').set({ type: 'FREE' }).where('id', '=', user_id).execute();
+  return Promise.all([
+    db
+      .updateTable('stripe_customer')
+      .set({ active_plan: null, subscription_id: null })
+      .where('subscription_id', '=', id)
+      .execute(),
+    db.updateTable('user').set({ type: 'FREE' }).where('id', '=', user_id).execute(),
+  ]);
 };
