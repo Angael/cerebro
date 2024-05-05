b3323851882b32bda692587e79fabadfff35f795
Angael <krzysztofwidacki@gmail.com>
2024-05-05 12:26:36 +0200

    colors for UsedSpace.tsx

--- apps/web/src/lib/used-space/UsedSpace.tsx
+++ apps/web/src/lib/used-space/UsedSpace.tsx
@@ -10,12 +10,22 @@ const UsedSpace = () => {
   const usageString =
     numeral(data?.bytes.used).format('0 b') + ' out of ' + numeral(data?.bytes.max).format('0 b');
 
-  const value = data ? (100 * data.bytes.used) / data.bytes.max : 0;
+  let value = data ? (100 * data.bytes.used) / data.bytes.max : 0;
+
+  if (data) {
+    if (data.bytes.max === 0) value = 100;
+    if (data.bytes.max > data.bytes.used) value = 100;
+  }
+
+  let color = undefined;
+  if (value > 70) color = 'yellow';
+  if (value > 90) color = 'red';
+
   return (
     <Paper p="md" pos="relative">
       <Stack>
         <Text>Used space: {usageString}</Text>
-        <Progress value={value} size="xl" />
+        <Progress value={value} size="xl" color={color} />
       </Stack>
       <LoadingOverlay visible={!data} />
     </Paper>
