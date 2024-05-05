cc5e0ee45c76319095681de66265f3ae1679f7ef
Angael <krzysztofwidacki@gmail.com>
2024-05-05 12:31:34 +0200

    block uploads for free users

--- apps/web/src/lib/files-preview/FilesPreview.tsx
+++ apps/web/src/lib/files-preview/FilesPreview.tsx
@@ -1,22 +1,24 @@
-import React, { useCallback } from "react";
-import FilePreview from "./FilePreview";
-import css from "./FilesPreview.module.scss";
-import { useDropzone } from "react-dropzone";
-import { mdiFileMultiple } from "@mdi/js";
-import { Icon } from "@mdi/react";
-import clsx from "clsx";
-import { ExtendedFile } from "@/app/upload/files/uploadTypes";
+import React, { useCallback } from 'react';
+import FilePreview from './FilePreview';
+import css from './FilesPreview.module.scss';
+import { useDropzone } from 'react-dropzone';
+import { mdiFileMultiple } from '@mdi/js';
+import { Icon } from '@mdi/react';
+import clsx from 'clsx';
+import { ExtendedFile } from '@/app/upload/files/uploadTypes';
 
 interface IProps {
   files: ExtendedFile[];
   onDelete: (id: string) => void;
   onAddFiles: (files: File[]) => void;
+  disabled?: boolean;
 }
 
-const FilesPreview = ({ files, onDelete, onAddFiles }: IProps) => {
+const FilesPreview = ({ files, onDelete, onAddFiles, disabled }: IProps) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop: onAddFiles,
     noClick: files.length > 0,
+    disabled,
   });
 
   return (
@@ -26,12 +28,13 @@ const FilesPreview = ({ files, onDelete, onAddFiles }: IProps) => {
         css.MyGrid,
         isDragActive && css.onDrag,
         files.length === 0 && css.canClick,
+        disabled && css.disabled,
       )}
     >
       {files.length === 0 && (
         <div className={css.dragWatermark}>
           <Icon path={mdiFileMultiple} size={3} />
-          <p>Drag & drop files</p>
+          {disabled ? <p>Only premium users can upload files</p> : <p>Drag & drop files</p>}
         </div>
       )}
       {files.map((file) => (
