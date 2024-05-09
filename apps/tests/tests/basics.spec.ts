4f5f8b04c597fcd2f692451316b305c140792562
Angael <Krzysztofwidacki@gmail.com>
2024-05-09 18:54:21 +0200

    Playwright (#11)
    * playwright first tests
    
    * ci/cd tests and split into more steps yml files
    
    * ci/cd tests and split into more steps yml files
    
    * ci/cd tests and split into more steps yml files
    
    * install playwright, optimize speed
    
    * install playwright, optimize speed
    
    * fix delete script
    
    * switch up pnpm install script
    
    * try with pnpm 9
    
    * remove rm
    
    * test test pipeline
    
    * test test pipeline
    
    * test test pipeline
    
    * fix tests
    
    * Optimize tests
    
    * Fix env
    
    * publish results from playwright
    
    * on fail
    
    * debug non existant files
    
    * only chromium
    
    * publish results
    
    * publish results
    
    * finallyyyyyy
    
    * test what happens when multiple things fail
    
    * Test what happens when all is good
    
    * zod tests; optimize build app

---
+++ apps/tests/tests/basics.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { testEnv } from './testEnv';
+
+test('Finds first item', async ({ page }) => {
+  await page.goto(`${testEnv.DOMAIN}/`);
+
+  await expect(page).toHaveTitle(/Cerebro/);
+  await expect(page.getByRole('link', { name: 'item' }).first()).toBeVisible();
+});
+
+test('Login page works', async ({ page }) => {
+  await page.goto(`${testEnv.DOMAIN}/signin`);
+
+  await page.getByRole('textbox', { name: 'email' }).fill(testEnv.TEST_EMAIL);
+  await page.getByRole('textbox', { name: 'password' }).fill(testEnv.TEST_PASS);
+  await page.click('button:has-text("Log in")');
+  await page.waitForURL(`${testEnv.DOMAIN}`);
+
+  await expect(page).not.toHaveURL('/signin');
+});
