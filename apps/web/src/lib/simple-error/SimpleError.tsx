e2aecbaae381ba44f3186d54fe38fc13e5bc20d0
Angael <krzysztofwidacki@gmail.com>
2024-05-18 01:59:05 +0200

    improve simple errors

--- apps/web/src/lib/simple-error/SimpleError.tsx
+++ apps/web/src/lib/simple-error/SimpleError.tsx
@@ -3,8 +3,11 @@
 import { useEffect } from 'react';
 import css from './SimpleError.module.scss';
 import { Stack, Text, Title } from '@mantine/core';
+import { parseErrorResponse } from '@/utils/parseErrorResponse';
 
-export default function SimpleError({ error }: { error: Error & { digest?: string } }) {
+export default function SimpleError({ error }: { error: any }) {
+  const errorMsg = parseErrorResponse(error);
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
@@ -15,16 +18,10 @@ export default function SimpleError({ error }: { error: Error & { digest?: strin
       <Title order={1}>Something went wrong</Title>
       <Text>Try reloading the page.</Text>
 
-      <details
-        className={css.details}
-        open={process.env.NODE_ENV !== 'production'}
-        style={!error.message ? { display: 'none' } : {}}
-      >
+      <details className={css.details} open={process.env.NODE_ENV !== 'production'}>
         <summary>Details</summary>
         <pre className={css.stack}>
-          {process.env.NODE_ENV === 'production'
-            ? `${error.name}: ${error.message}`
-            : error.stack?.split('\n').map((line, i) => <Text key={i}>{line}</Text>)}
+          {error.name ?? 'Unknown'}: {JSON.stringify(errorMsg, null, 2)}
         </pre>
       </details>
     </Stack>
