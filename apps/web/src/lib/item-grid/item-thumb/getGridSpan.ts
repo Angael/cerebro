43f3bf9172209aa6695f846fdf72820eed8ae9c6
Angael <krzysztofwidacki@gmail.com>
2024-05-25 23:03:52 +0200

    fix 2 bugs

--- apps/web/src/lib/item-grid/item-thumb/getGridSpan.ts
+++ apps/web/src/lib/item-grid/item-thumb/getGridSpan.ts
@@ -2,15 +2,19 @@ import type { FrontItem } from '@cerebro/shared';
 import { MD_CELL_SIZE } from '@/utils/consts';
 
 const getWH = (item: FrontItem): { w: number; h: number } => {
-  if (item.type === 'VIDEO') {
-    const { width: w, height: h } = item.videos.find((v) => v.mediaType === 'SOURCE')!;
-    return { w, h };
+  try {
+    if (item.type === 'VIDEO') {
+      const { width: w, height: h } = item.videos.find((v) => v.mediaType === 'SOURCE')!;
+      return { w, h };
+    }
+    if (item.type === 'IMAGE') {
+      const { width: w, height: h } = item.images.find((i) => i.mediaType === 'SOURCE')!;
+      return { w, h };
+    }
+    return { w: 0, h: 0 };
+  } catch (e) {
+    return { w: 0, h: 0 };
   }
-  if (item.type === 'IMAGE') {
-    const { width: w, height: h } = item.images.find((i) => i.mediaType === 'SOURCE')!;
-    return { w, h };
-  }
-  return { w: 0, h: 0 };
 };
 
 export const getGridSpan = (item: FrontItem): '' | 'tall' | 'wide' => {
