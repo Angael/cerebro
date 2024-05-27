fe47a0d148b8f65a49cc246f4fc222127ecc4e7e
Angael <krzysztofwidacki@gmail.com>
2024-05-27 20:31:53 +0200

    remove logs

--- apps/web/src/lib/video-player/VideoPlayer.tsx
+++ apps/web/src/lib/video-player/VideoPlayer.tsx
@@ -60,7 +60,6 @@ const VideoPlayer = ({
   };
 
   const handleSeek = (progressFromSlider: number) => {
-    console.log('handleSeek', progressFromSlider);
     ref.current?.seekTo(progressFromSlider, 'fraction');
     startTransition(() => {
       setPlaying(false);
@@ -194,13 +193,8 @@ const VideoPlayer = ({
           onClick={(e) => e.stopPropagation()}
           onMouseDown={(e) => {
             e.stopPropagation();
-            console.log('onMouseDown');
-
             seekContinuePlaying.current = playing;
             setPlaying(false);
-          }}
-          onMouseUp={(e) => {
-            console.log('onMouseUp');
           }}
           style={{ flex: 1 }}
         />
