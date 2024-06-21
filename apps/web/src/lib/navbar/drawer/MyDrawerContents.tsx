e2b48ba55d803358ac86662925e4dfa3cd7a8c7f
Angael <krzysztofwidacki@gmail.com>
2024-06-21 19:16:55 +0200

    Add bg to side drawer

--- apps/web/src/lib/navbar/drawer/MyDrawerContents.tsx
+++ apps/web/src/lib/navbar/drawer/MyDrawerContents.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack } from '@mantine/core';
+import { Paper, Stack } from '@mantine/core';
 import css from './MyDrawerContents.module.scss';
 import { RouteNavLink } from '@/lib/route-nav-link/RouteNavLink';
 import { useCurrentUser } from '@/utils/hooks/useCurrentUser';
@@ -20,28 +20,30 @@ const MyDrawerContents = ({ onClose }: Props) => {
   });
 
   return (
-    <Stack gap="0" className={css.MyDrawerContents}>
-      <RouteNavLink href="/browse" label="Browse" onClick={onClose} />
-      {user.data && <RouteNavLink href="/upload" label="Upload" onClick={onClose} />}
+    <Paper p="md" className={css.MyDrawerContents}>
+      <Stack gap="0" h="100%">
+        <RouteNavLink href="/browse" label="Browse" onClick={onClose} />
+        {user.data && <RouteNavLink href="/upload" label="Upload" onClick={onClose} />}
 
-      <div style={{ flex: 1 }}></div>
+        <div style={{ flex: 1 }}></div>
 
-      {user.data && <RouteNavLink href="/account" label="My account" onClick={onClose} />}
-      {user.data ? (
-        <RouteNavLink
-          href="/signin"
-          label="Logout"
-          onClick={() => {
-            logout.mutate();
-            onClose?.();
-          }}
-          style={{ opacity: logout.isPending ? 0.5 : 1 }}
-        />
-      ) : (
-        <RouteNavLink href="/signin" label="Login" onClick={onClose} />
-      )}
-      {user.data?.type === 'ADMIN' && <RouteNavLink href="/admin" label="Admin panel" />}
-    </Stack>
+        {user.data && <RouteNavLink href="/account" label="My account" onClick={onClose} />}
+        {user.data ? (
+          <RouteNavLink
+            href="/signin"
+            label="Logout"
+            onClick={() => {
+              logout.mutate();
+              onClose?.();
+            }}
+            style={{ opacity: logout.isPending ? 0.5 : 1 }}
+          />
+        ) : (
+          <RouteNavLink href="/signin" label="Login" onClick={onClose} />
+        )}
+        {user.data?.type === 'ADMIN' && <RouteNavLink href="/admin" label="Admin panel" />}
+      </Stack>
+    </Paper>
   );
 };
 
