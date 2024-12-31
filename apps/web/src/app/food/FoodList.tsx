6bfdb978912e1a76987d4b3ffa46036e9056f480
Angael <krzysztofwidacki@gmail.com>
2024-12-31 12:08:59 +0100

    Enhance FoodList component: add message for no logged foods and wrap food items in a Stack for better layout

--- apps/web/src/app/food/FoodList.tsx
+++ apps/web/src/app/food/FoodList.tsx
@@ -1,19 +1,25 @@
 import { QueryFoodToday } from '@cerebro/server/src/routes/food/food.routes';
-import { List, ListItem } from '@mantine/core';
+import { List, ListItem, Stack, Text } from '@mantine/core';
 
 type Props = {
   foods: QueryFoodToday;
 };
 
 const FoodList = ({ foods }: Props) => {
+  if (foods.length === 0) {
+    return <Text>No foods logged today</Text>;
+  }
   return (
-    <List>
-      {foods.map((food) => (
-        <ListItem key={food.id}>
-          {food.product_name} {food.brands} {food.barcode} {food.amount}g/ml {food.kcal}kcal
-        </ListItem>
-      ))}
-    </List>
+    <Stack>
+      <Text>Logged calories:</Text>
+      <List>
+        {foods.map((food) => (
+          <ListItem key={food.id}>
+            {food.product_name} {food.brands} {food.barcode} {food.amount}g/ml {food.kcal}kcal
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
   );
 };
 
