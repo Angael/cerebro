8f05b224159c2a8aa9fd920870f25a882f83b865
Angael <krzysztofwidacki@gmail.com>
2025-03-02 18:15:59 +0100

    Implement food log entry collapse feature: add context for managing open state, enhance UI with buttons for adding, editing, and deleting food logs

--- apps/web/src/app/food/food-log-entry/FoodLogEntry.tsx
+++ apps/web/src/app/food/food-log-entry/FoodLogEntry.tsx
@@ -1,31 +1,57 @@
 import { QueryFoodToday } from '@cerebro/server/src/routes/food/food.model';
-import { Group, Stack, Text, UnstyledButton } from '@mantine/core';
+import { Button, Collapse, Group, Stack, Text, UnstyledButton } from '@mantine/core';
 import css from './FoodLogEntry.module.css';
+import { useFoodLogsContext } from './FoodLogsContext';
+import Icon from '@mdi/react';
+import { mdiPen, mdiPencil } from '@mdi/js';
 
 type Props = {
   food: QueryFoodToday[number];
 };
 
 const FoodLogEntry = ({ food }: Props) => {
+  const { openFoodLogId, setOpenFoodLogId } = useFoodLogsContext();
+  const isOpen = openFoodLogId === food.id;
+
   return (
-    <UnstyledButton component="li" role="button" className={css.unstyledLi} tabIndex={0}>
-      <Group align="flex-start" justify="space-between">
-        <Stack component="header" gap={0} flex={3}>
-          <Text size="md" fw="bold">
-            {food.product_name}
-          </Text>
-          <Text size="sm">
-            {food.brands}{' '}
-            <Text span c="dark.2">
-              {food.amount}g
+    <li className={css.unstyledLi}>
+      <UnstyledButton
+        className={css.btn}
+        onClick={() => {
+          if (isOpen) {
+            setOpenFoodLogId(null);
+          } else {
+            setOpenFoodLogId(food.id);
+          }
+        }}
+      >
+        <Group className={css.hoverAnimation} align="flex-start" justify="space-between">
+          <Stack component="header" gap={0} flex={3}>
+            <Text size="md" fw="bold">
+              {food.product_name}
             </Text>
-          </Text>
-        </Stack>
-        <Stack align="flex-end" gap={0} flex={1}>
-          <Text size="md">{Math.ceil(food.kcal)} kcal</Text>
-        </Stack>
-      </Group>
-    </UnstyledButton>
+            <Text size="sm">
+              {food.brands}{' '}
+              <Text span c="dark.2">
+                {food.amount}g
+              </Text>
+            </Text>
+          </Stack>
+          <Stack align="flex-end" gap={0} flex={1}>
+            <Text size="md">{Math.ceil(food.kcal)} kcal</Text>
+          </Stack>
+        </Group>
+      </UnstyledButton>
+      <Collapse in={isOpen}>
+        <Group pb="xs">
+          <Button>Add today</Button>
+          <Button variant="outline" leftSection={<Icon path={mdiPencil} size={1} />}>
+            Edit
+          </Button>
+          <Button color="red">Delete</Button>
+        </Group>
+      </Collapse>
+    </li>
   );
 };
 
--- apps/web/src/app/food/food-log-entry/FoodLogsList.tsx
+++ apps/web/src/app/food/food-log-entry/FoodLogsList.tsx
@@ -1,10 +1,17 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 import css from './FoodLogsList.module.css';
+import { FoodLogsContextProvider } from './FoodLogsContext';
 
 type Props = ComponentProps<'ul'>;
 
 const FoodLogsList = ({ className, ...props }: Props) => {
-  return <ul className={css.foodLogsList} {...props} />;
+  const [openFoodLogId, setOpenFoodLogId] = useState<string | null>(null);
+
+  return (
+    <FoodLogsContextProvider value={{ openFoodLogId, setOpenFoodLogId }}>
+      <ul className={css.foodLogsList} {...props} />
+    </FoodLogsContextProvider>
+  );
 };
 
 export default FoodLogsList;
