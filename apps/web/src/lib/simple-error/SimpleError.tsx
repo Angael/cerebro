6ee948347754f514b333c12c47f8cb8a4f1b5e61
Angael <krzysztofwidacki@gmail.com>
2025-04-24 20:31:28 +0200

    Refactor SimpleError component to use Alert instead of Stack and update styles for better presentation

--- apps/web/src/lib/simple-error/SimpleError.tsx
+++ apps/web/src/lib/simple-error/SimpleError.tsx
@@ -1,10 +1,10 @@
 'use client'; // Error components must be Client Components
 
+import { parseErrorResponse } from '@/utils/parseErrorResponse';
+import { Alert, Text } from '@mantine/core';
+import * as Sentry from '@sentry/nextjs';
 import { useEffect } from 'react';
 import css from './SimpleError.module.scss';
-import { Stack, Text, Title } from '@mantine/core';
-import { parseErrorResponse } from '@/utils/parseErrorResponse';
-import * as Sentry from '@sentry/nextjs';
 
 export default function SimpleError({ error }: { error: any }) {
   const errorMsg = parseErrorResponse(error);
@@ -14,9 +14,8 @@ export default function SimpleError({ error }: { error: any }) {
   }, [error]);
 
   return (
-    <Stack component="article" justify="center" align="center" flex={1}>
-      <Title order={1}>Something went wrong</Title>
-      <Text>Try reloading the page.</Text>
+    <Alert color="red" title="Something went wrong">
+      <Text mb="xs">Try reloading the page.</Text>
 
       <details className={css.details} open={process.env.NODE_ENV !== 'production'}>
         <summary>Details</summary>
@@ -24,6 +23,6 @@ export default function SimpleError({ error }: { error: any }) {
           {error.name ?? 'Unknown'}: {JSON.stringify(errorMsg, null, 2)}
         </pre>
       </details>
-    </Stack>
+    </Alert>
   );
 }
