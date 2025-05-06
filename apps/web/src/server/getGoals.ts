206d8503fae30e49eb2ac1bc2b23d2ce049393ed
Angael <krzysztofwidacki@gmail.com>
2025-05-07 00:39:52 +0200

    Add Sentry tracing to getGoals function for improved performance monitoring

--- apps/web/src/server/getGoals.ts
+++ apps/web/src/server/getGoals.ts
@@ -1,19 +1,21 @@
 'use server';
 
 import { db } from '@cerebro/db';
+import { startSpan } from '@sentry/nextjs';
 import { sql } from 'kysely';
 
 export type GoalsType = Awaited<ReturnType<typeof getGoals>>;
 
-export const getGoals = async (userId: string) => {
-  return (
-    db
-      .selectFrom('food_goal')
-      .select(['weight_kg', 'kcal', 'date'])
-      .where('user_id', '=', userId)
-      // First entry with date lower or equal to the date
-      .where(sql<any>`DATE(date) <= ${new Date().toISOString().split('T')[0]!}`)
-      .orderBy('date', 'desc')
-      .executeTakeFirst()
-  );
-};
+export const getGoals = async (userId: string) =>
+  startSpan({ name: 'SF_getGoals', op: 'db' }, async () => {
+    return (
+      db
+        .selectFrom('food_goal')
+        .select(['weight_kg', 'kcal', 'date'])
+        .where('user_id', '=', userId)
+        // First entry with date lower or equal to the date
+        .where(sql<any>`DATE(date) <= ${new Date().toISOString().split('T')[0]!}`)
+        .orderBy('date', 'desc')
+        .executeTakeFirst()
+    );
+  });
