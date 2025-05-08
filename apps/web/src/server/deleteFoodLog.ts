6f110bbb1444bd015c651cef1e338eb7f5ba8848
Angael <krzysztofwidacki@gmail.com>
2025-05-08 23:42:36 +0200

    Add Sentry tracing to deleteFoodLog function for improved monitoring

--- apps/web/src/server/deleteFoodLog.ts
+++ apps/web/src/server/deleteFoodLog.ts
@@ -2,18 +2,18 @@
 import { z } from 'zod';
 import { requireUser } from './getUser';
 import { db } from '@cerebro/db';
+import { startSpan } from '@sentry/nextjs';
 
 const zFoodId = z.number().int();
 
 export const deleteFoodLog = async (foodId: number) => {
   const user = await requireUser();
+
   const foodLogId = zFoodId.parse(foodId);
 
-  await db
-    .deleteFrom('food_log')
-    .where('user_id', '=', user.id)
-    .where('id', '=', foodLogId)
-    .execute();
+  await startSpan({ name: 'SF_deleteFoodLog', op: 'db' }, () =>
+    db.deleteFrom('food_log').where('user_id', '=', user.id).where('id', '=', foodLogId).execute(),
+  );
 
   return null;
 };
