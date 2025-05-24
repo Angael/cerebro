3fbf68972ad2dc2418125ecd12085f37c06912d3
Angael <krzysztofwidacki@gmail.com>
2025-05-24 13:24:56 +0200

    Enhance RouteNavLink to manually prefetch routes on hover

--- apps/web/src/lib/route-nav-link/RouteNavLink.tsx
+++ apps/web/src/lib/route-nav-link/RouteNavLink.tsx
@@ -1,19 +1,28 @@
 import Link, { LinkProps } from 'next/link';
 import { NavLink, NavLinkProps } from '@mantine/core';
 import React from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 type Props = {} & LinkProps & NavLinkProps;
 
 export const RouteNavLink = ({ href, ...props }: Props) => {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleMouseEnter = () => {
+    // Prefetch the route on hover
+    router.prefetch(href.toString());
+  };
 
   return (
     <NavLink
+      // prefetch={false} - we handle prefetching manually on hover to avoid issues with dynamic search params
       component={Link}
       active={pathname === href.toString()}
       variant={'light'}
       href={href}
+      onMouseEnter={handleMouseEnter}
+      prefetch={false}
       {...props}
     />
   );
