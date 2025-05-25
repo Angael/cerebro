01c05f26e02287d699ca9f851c91f653a2d2dbfd
Angael <krzysztofwidacki@gmail.com>
2025-05-25 11:28:41 +0200

    Replace development favicon PNG with ICO format and update layout to use the new favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             </>
           ) : (
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32 dev.png" />
+            <link rel="shortcut icon" href="/favicon-dev.ico" />
           )}
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
