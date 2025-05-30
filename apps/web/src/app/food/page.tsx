16c5990b67b5a6c4ea682b14c43ac4c2cb2955f7
Angael <krzysztofwidacki@gmail.com>
2025-05-30 02:06:48 +0200

    Refactor FoodLogsContext and FoodLogsList: move context provider logic to FoodLogsContext and clean up FoodLogsList component

--- apps/web/src/app/food/page.tsx
+++ apps/web/src/app/food/page.tsx
@@ -2,13 +2,18 @@ import { requireUser } from '@/server/auth/getUser';
 import Food from './Food';
 import { getGoals } from '@/server/getGoals';
 import { getFoodHistory } from '@/server/getFoodHistory';
+import { FoodLogsContextProvider } from './food-log-entry/FoodLogsContext';
 
 const FoodPage = async () => {
   const user = await requireUser();
   const goals = getGoals(user.id);
   const foodHistory = getFoodHistory(user.id);
 
-  return <Food goals={await goals} foodHistoryInit={await foodHistory} />;
+  return (
+    <FoodLogsContextProvider>
+      <Food goals={await goals} foodHistoryInit={await foodHistory} />;
+    </FoodLogsContextProvider>
+  );
 };
 
 export default FoodPage;
